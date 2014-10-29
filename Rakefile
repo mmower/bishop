243d7a22d4e84a6602c544a3938168d0a928c520
Richard Harrington <tamsin@maymount.com>
2014-10-28 19:41:33 -0700

    Adding Rakefile, Gemfile, and some tests

---
+++ Rakefile
@@ -0,0 +1,10 @@
+require 'rake/testtask'
+
+#desc "Default task: test"
+#task :default => [:test_unit]
+
+desc "Run Tests"
+Rake::TestTask.new( :test ) do |t|
+  t.pattern = "test/test_*.rb"
+  t.verbose = true
+end
---
+++ test/test_tokenizers.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'bundler'
+require 'minitest'
+require 'minitest/autorun'
+Bundler.require(:default, :test)
+
+require_relative '../lib/bayes/bishop'
+
+class TestTokenizers < Minitest::Test
+  parallelize_me!
+
+  def test_simple_tokenizer
+    tokenizer = Bishop::SimpleTokenizer.new
+    s1 = '  this " :-) ;.; % $ &*# is a hyPhen-Test to see - what happens --  '
+    r1 = ["this", "is", "a", "hyphen-test", "to", "see", "what", "happens"] 
+    assert_equal r1, tokenizer.tokenize(s1)
+  end
+  
+  def test_simple_tokenizer_stop_words
+    tokenizer = Bishop::SimpleTokenizer.new
+    s1 = '  alpha beta delta gamma omega phi psi tau  '
+    r1 = %w( alpha beta gamma omega psi tau )
+    assert_equal r1, tokenizer.tokenize(s1,%w(delta phi))
+  end
+  
+end
