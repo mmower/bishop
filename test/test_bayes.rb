7275efcea7cbc125aece788807b9bc2d034cf34d
Richard Harrington <tamsin@maymount.com>
2014-10-29 09:26:17 -0700

    Basic tests working

--- test/test_bayes.rb
+++ test/test_bayes.rb
@@ -38,6 +38,10 @@ class TestBayes < Minitest::Test
     refute_equal 0, b.stop_words.length
   end
   
+  def test_load_stop_words
+    skip
+  end
+  
   def test_new_pool
     b = Bishop::Bayes.new
     p = b.new_pool('testing')
@@ -57,4 +61,80 @@ class TestBayes < Minitest::Test
     refute b.pools.has_key?('testing')
     assert b.pools.has_key?('gnitset')
   end
+  
+  def test_pool_names
+    b = Bishop::Bayes.new
+    names1 = %w{ gamma alpha beta}
+    names1.each { |n| b.new_pool(n) }
+    assert_equal names1.sort, b.pool_names
+  end
+  
+  def test_merge_pools
+    skip
+  end
+  
+  def test_save_state
+    skip
+  end
+
+  def test_train_simple
+    b = Bishop::Bayes.new
+    l1 = "Four score and seven years ago our fathers brought forth on this continent,"+
+      " a new nation, conceived in Liberty, and dedicated to the proposition that all"+
+      " men are created equal."
+    l2 ="Now we are engaged in a great civil war, testing whether that nation, "+
+      "or any nation so conceived and so dedicated, can long endure. We are met on"+
+      " a great battle-field of that war. We have come to dedicate a portion of that"+
+      " field, as a final resting place for those who here gave their lives that that"+
+      " nation might live. It is altogether fitting and proper that we should do this."
+    l3 = "But, in a larger sense, we can not dedicate -- we can not consecrate -- we can not hallow --"+
+    " this ground. "
+    l4 = "The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. "
+    
+    j1 = "Beware the Jabberwock, my son!"+
+      " The jaws that bite, the claws that catch!"+
+      " Beware the Jubjub bird, and shun The frumious Bandersnatch!"
+    j2 = "He took his vorpal sword in hand:"+
+      " Long time the manxome foe he sought -- " +
+      " So rested he by the Tumtum tree, " +
+      " And stood awhile in thought. "
+    j3 = "And, as in uffish thought he stood, " +
+      " The Jabberwock, with eyes of flame, " +
+      " Came whiffling through the tulgey wood, " +
+      " And burbled as it came!"  
+    j4 = "One, two! One, two! And through and through" +
+      " The vorpal blade went snicker-snack! " +
+      " He left it dead, and with its head " +
+      " He went galumphing back." 
+      
+      r = "Two households, both alike in dignity, "+
+            "In fair Verona, where we lay our scene, "+
+            "From ancient grudge break to new mutiny, "
+            
+    b.load_default_stop_words
+    
+    b.train('lincoln', l1)
+    b.train('lincoln', l2)
+    b.train('lincoln', l3)
+    
+    b.train('jabber', j1)
+    b.train('jabber', j2)
+    b.train('jabber', j3)
+    
+    guess_lincoln = b.guess(l4)
+    
+    guess_jabber = b.guess(j4)
+    
+    guess_romeo = b.guess(r)
+    
+    assert guess_lincoln.has_key?('lincoln')
+    assert guess_jabber.has_key?('jabber')
+    refute guess_romeo.has_key?('lincoln')
+    refute guess_romeo.has_key?('jabber')
+
+    assert guess_lincoln['lincoln'] > 0.9
+    assert guess_jabber['jabber'] > 0.9
+  end
+  
+  
 end
